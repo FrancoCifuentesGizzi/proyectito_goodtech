db1c6fcf90b28b9dcd2982a90fbe1cd225dac81e
Stressed-Cat <ze.aqua.doggo@gmail.com>
2021-12-12 04:20:27 -0300

    Ahora si :^) funciona

--- empleado.py
+++ empleado.py
@@ -45,16 +45,17 @@ class empleado:
         ttk.Button(self.root, command = self.__Eliminar_E, text="Eliminar").place(x = 600, y = 350, width = 300, height = 50)
 
     def llenar_treeview_empleado(self):
-        sql = """select id_empleado, nombre_emp, apellido_emp, telefono_emp, sucursal.nombre_suc, perfil.tipo_perfil
-            from empleado join sucursal on empleado.sucursal_id_sucursal = sucursal.id_sucursal
-            join perfil on empleado.perfil_id_perfil = perfil.id_perfil order by id_empleado asc"""
+        sql = """select id_empleado, nombre_emp, apellido_emp, telefono_emp, sucursal.nombre_suc, perfil.tipo_perfil from empleado
+            join sucursal on empleado.sucursal_id_sucursal = sucursal.id_sucursal
+            join perfil on empleado.perfil_id_perfil = perfil.id_perfil
+            order by id_empleado asc"""
         data = self.db.run_select ( sql )
 
         if (data != self.data):
             self.treeview.delete ( *self.treeview.get_children () )  # Elimina todos los rows del treeview
             for i in data:
                 self.treeview.insert ( "", "end", text=i[0],
-                                       values=(i[1], i[2], i[3], i[4]), iid=i[0] )
+                                       values=(i[1], i[2], i[3], i[4], i[5]), iid=i[0] )
             self.data = data
 
     def __Agregar_E(self):
@@ -62,16 +63,17 @@ class empleado:
 
     def __Editar_E(self):
         if (self.treeview.focus () != ""):
-            sql = """select id_empleado, nombre_emp, apellido_emp, telefono_emp, sucursal.nombre_suc from empleado
+            sql = """select id_empleado, nombre_emp, apellido_emp, telefono_emp, sucursal.nombre_suc, perfil.tipo_perfil from empleado
                 join sucursal on empleado.sucursal_id_sucursal = sucursal.id_sucursal
-                join perfil on empleado.perfil_id_perfil = perfil.id_perfil where id_empleado = %(id_empleado)s"""
+                join perfil on empleado.perfil_id_perfil = perfil.id_perfil
+                where id_empleado = %(id_empleado)s"""
             row_data = self.db.run_select_filter ( sql, {"id_empleado": self.treeview.focus ()} )[0]
             editar_empleado ( self.db, self, row_data )
 
     def __Eliminar_E(self):
         sql = "select * from empleado where id_empleado = %(id_empleado)s"
-        row_data = self.db.run_select_filter ( sql, {"id_sucursal": self.treeview.focus ()} )[0]
-        Eliminar_Sucursal( self.db, self, row_data )
+        row_data = self.db.run_select_filter ( sql, {"id_empleado": self.treeview.focus ()} )[0]
+        Eliminar_Empleado( self.db, self, row_data )
 
 class Add_Empleado:
     #Configuración de la ventana agregar
@@ -107,33 +109,33 @@ class Add_Empleado:
         self.entry_telefono.place(x = 100, y = 60, width = 100, height = 20)
         self.combosucursal = ttk.Combobox(self.add)
         self.combosucursal.place(x = 100, y = 85, width = 100, height = 20)
-        self.combosucursal["values"], self.ids = self.__fill_combo_sucursal ()
+        self.combosucursal["values"], self.suc = self.__fill_combo_sucursal ()
         self.comboperfil = ttk.Combobox(self.add)
         self.comboperfil.place(x = 100, y = 110, width = 100, height = 20)
-        self.comboperfil["values"], self.ids = self.__fill_combo_perfil ()
+        self.comboperfil["values"], self.per = self.__fill_combo_perfil ()
 
     #Configuración de los botones
     def __config_buttons(self):
         ttk.Button(self.add, command = self.__insertar, text="Aceptar").place(x = 70, y = 135, width = 90, height = 30)
 
     def __fill_combo_sucursal(self):
-        sql = "select id_sucursal, nombre_suc from sucursal"
+        sql = "select id_sucursal, nombre_suc from sucursal order by id_sucursal asc"
         self.data = self.db.run_select ( sql )
         return [i[1] for i in self.data], [i[0] for i in self.data]
 
     def __fill_combo_perfil(self):
-        sql = "select id_perfil, tipo_perfil from perfil"
+        sql = "select id_perfil, tipo_perfil from perfil order by id_perfil asc"
         self.data = self.db.run_select ( sql )
         return [i[1] for i in self.data], [i[0] for i in self.data]
 
     def __insertar(self):  # Insercion en la base de datos.
         sql = """insert empleado (nombre_emp, apellido_emp, telefono_emp, sucursal_id_sucursal, perfil_id_perfil)
-                values (%(nombre_emp)s, %(apellido_emp)s, %(telefono_emp)s, %(sucursal_id_sucursal)s, %(perfil_id_perfil)s);"""
+                values (%(nombre_emp)s, %(apellido_emp)s, %(telefono_emp)s, %(sucursal_id_sucursal)s, %(perfil_id_perfil)s)"""
         self.db.run_sql ( sql, {"nombre_emp": self.entry_nombre.get (),
                                 "apellido_emp": self.entry_apellido.get (),
                                 "telefono_emp": self.entry_telefono.get (),
-                                "sucursal_id_sucursal": self.ids[self.combosucursal.current()],
-                                "perfil_id_perfil": self.ids[self.comboperfil.current ()]})
+                                "sucursal_id_sucursal": self.suc[self.combosucursal.current()],
+                                "perfil_id_perfil": self.per[self.comboperfil.current()]})
         self.add.destroy ()
         self.padre.llenar_treeview_empleado()
 
@@ -173,10 +175,10 @@ class editar_empleado:  # Clase para modificar
         self.entry_telefono.place(x = 100, y = 100, width = 120, height = 20)
         self.combosuc = ttk.Combobox(self.insert_datos)
         self.combosuc.place(x = 100, y = 130, width = 120, height = 20)
-        self.combosuc["values"], self.ids = self.fill_combosuc ()
+        self.combosuc["values"], self.suc = self.fill_combosuc ()
         self.comboper = ttk.Combobox(self.insert_datos)
         self.comboper.place(x = 100, y = 160, width = 120, height = 20)
-        self.comboper["values"], self.ids = self.fill_comboper ()
+        self.comboper["values"], self.per = self.fill_comboper ()
         self.entry_nombre.insert ( 0, self.row_data[1] )
         self.entry_apellido.insert ( 0, self.row_data[2] )
         self.entry_telefono.insert ( 0, self.row_data[3] )
@@ -191,24 +193,24 @@ class editar_empleado:  # Clase para modificar
 
     def modificar(self):  # Insercion en la base de datos.
         sql = """update empleado set nombre_emp = %(nombre_emp)s, apellido_emp = %(apellido_emp)s,
-            telefono_emp = %(telefono_emp)s, sucursal_id_sucursal = %(id_sucursal)s, perfil_id_perfil = %(id_perfil)
-            where id_empleado = %(id_empleado)s"""
+                telefono_emp = %(telefono_emp)s, sucursal_id_sucursal = %(id_sucursal)s, perfil_id_perfil = %(id_perfil)s
+                where id_empleado = %(id_empleado)s"""
         self.db.run_sql ( sql, {"nombre_emp": self.entry_nombre.get (),
                                 "apellido_emp": self.entry_apellido.get (),
                                 "telefono_emp": self.entry_telefono.get (),
-                                "id_sucursal": self.ids[self.combosuc.current ()],
-                                "id_perfil": self.ids[self.comboper.current ()],
+                                "id_sucursal": self.suc[self.combosuc.current ()],
+                                "id_perfil": self.per[self.comboper.current ()],
                                 "id_empleado": self.row_data[0]} )
         self.insert_datos.destroy ()
         self.padre.llenar_treeview_empleado ()
 
     def fill_combosuc(self):  #
-        sql = "select id_sucursal, nombre_suc from sucursal"
+        sql = "select id_sucursal, nombre_suc from sucursal order by id_sucursal asc"
         self.data = self.db.run_select ( sql )
         return [i[1] for i in self.data], [i[0] for i in self.data]
 
     def fill_comboper(self):  #
-        sql = "select id_perfil, tipo_perfil from perfil"
+        sql = "select id_perfil, tipo_perfil from perfil order by id_perfil asc"
         self.data = self.db.run_select ( sql )
         return [i[1] for i in self.data], [i[0] for i in self.data]
 
@@ -232,8 +234,8 @@ class Eliminar_Empleado:
     def __config_label(self):
         tk.Label ( self.del_datos, text= "Se eliminará los siguientes datos: ").place ( x=5, y=10, width=250, height=20 )
         tk.Label ( self.del_datos, text= "Nombre: " + (self.row_data[1])).place( x=5, y=30, width=250, height=20 )
-        tk.Label ( self.del_datos, text= "Apellido: " + (self.row_data[1])).place( x=5, y=30, width=250, height=20 )
-        # tk.Label ( self.del_datos, text= "Sucursal: " + (self.row_data[1])).place( x=5, y=30, width=250, height=20 ) # el id es int
+        tk.Label ( self.del_datos, text= "Apellido: " + (self.row_data[1])).place( x=5, y=50, width=250, height=20 )
+        # tk.Label ( self.del_datos, text= "Sucursal: " + (self.row_data[1])).place( x=5, y=70, width=250, height=20 ) # el id es int
 
     def __config_button(self):
         ttk.Button(self.del_datos, command = self.__Cancelar, text="Cancelar").place(x = 0, y = 80, width = 100, height = 50)
@@ -246,4 +248,4 @@ class Eliminar_Empleado:
         sql = "delete from empleado where id_empleado = %(id_empleado)s"
         self.db.run_sql ( sql, {"id_empleado": int ( self.row_data[0] )} )
         self.del_datos.destroy ()
-        self.padre.llenar_treeview_perfil ()
+        self.padre.llenar_treeview_empleado ()
